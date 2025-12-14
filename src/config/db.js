f07553cbc88bf2de48c65a7f9578f540912b7ed4
Umeshh27 <23MH1A05E7@acoe.edu.in>
2025-12-14 17:34:14 +0530

    update db.js to use hardcoded database connection parameters instead of environment variable

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  user: "postgres",
+  host: "localhost",
+  database: "social_feed",
+  password: "Umesh2006",
+  port: 5432,
 });
 
 export default pool;
