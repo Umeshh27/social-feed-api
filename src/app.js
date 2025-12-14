d85b020bfe379f6ad940bd14244a4d94056d279b
Umeshh27 <23MH1A05E7@acoe.edu.in>
2025-12-14 17:53:13 +0530

    add user routes to the application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Social Feed API is running");
